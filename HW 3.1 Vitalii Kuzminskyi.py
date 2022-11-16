d09bdf7c67fab11ff8ee553545254dce63c59559
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2022-11-16 21:36:39 +0200

    HW 3.1 and 3.2 без валидации

--- HW 3.1 Vitalii Kuzminskyi.py
+++ HW 3.1 Vitalii Kuzminskyi.py
@@ -1,8 +1,12 @@
-# HW 3.1
+# HW 3.1-3.2
 
-a = float(input('Пожалуйста введите первое число: '))
+
+a = float(input('Пожалуйста введите первое число: ')) #использовал float для более широких возможностей и более точного расчёта
 b = float(input('Пожалуйста введите второе число: '))
 c = float(input('Пожалуйста введите третье число: '))
-g= a, b, c
-#print('Yes'if ((a, b, c) > 10) else  'No')
-print('Yes'if g > 10 else 'No')
+
+max = max(a, b, c)
+
+if (a > 10 and b > 10 and c > 10) and ( a % 3 == 0 and b % 3 == 0): print (' Yes - Homework 3.1\n',max, '- Homework 3.2')
+
+else: print(' No - Homework 3.1\n', max, '- Homework 3.2')
