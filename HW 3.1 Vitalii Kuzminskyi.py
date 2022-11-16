8127b8089c2fb56aba2eed7be7f7540e5f0584d6
Vitalii <vitalii.kuzminskyi@gmail.com>
2022-11-16 08:18:33 +0200

    HW 3.1

--- HW 3.1 Vitalii Kuzminskyi.py
+++ HW 3.1 Vitalii Kuzminskyi.py
@@ -3,5 +3,6 @@
 a = float(input('Пожалуйста введите первое число: '))
 b = float(input('Пожалуйста введите второе число: '))
 c = float(input('Пожалуйста введите третье число: '))
-
-print('Hello', a , b,', how can I halp you?')
+g= a, b, c
+#print('Yes'if ((a, b, c) > 10) else  'No')
+print('Yes'if g > 10 else 'No')
