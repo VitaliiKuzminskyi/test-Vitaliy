daef026ccf605ba2582d2cdb0eed0dcb43c1de56
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2022-11-18 01:21:48 +0200

    задача с 1/4 часа

---
+++ 1/1_4 hour task.py
@@ -0,0 +1,15 @@
+a = float(input('Введите значение минуты: '))
+b = 59
+
+
+if 0 <= a <= (b / 4):
+    print('First')
+elif (b / 4) <= a <= (b / 2):
+    print('Second')
+elif (b / 2) <= a <= ( (3 * b) / 4):
+    print('Third')
+elif ( (3 * b) / 4) <= a <= b:
+    print('Fourth')
+
+else:
+    print('Error')
