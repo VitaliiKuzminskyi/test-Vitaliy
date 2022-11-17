b09dc5b774af80fb88a9d364ce7ce46c03739d4b
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2022-11-18 01:02:46 +0200

    HW3+ без валидации на текст

--- HW 3+.py
+++ HW 3+.py
@@ -1,15 +1,19 @@
 
-number = int(input('Пожалуйста введите целое трёхзначное число: '))
+while True:
+    try:
+        number = int(input('Пожалуйста введите целое трёхзначное число: '))
+        break
+    except:
+        print('Ввод символов кроме цифр ЗАПРЕЩЁН, введите трёхзначное число: ')
 
 if 100 <= number <= 999:
-    reversed_number = 0
+    a = 0
     while number > 0:
         z = number % 10
         number //= 10
-        reversed_number *= 10
-        reversed_number += z
-    print(reversed_number)
+        a *= 10
+        a += z
 
+    reversed_number = a
 
-else:
-    print('No')
+print(reversed_number)
