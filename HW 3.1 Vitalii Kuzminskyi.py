39ef59654e3413dc54e5946f1ba3b63eab6734f5
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2022-11-17 22:33:15 +0200

    HW 3.1 and 3.2 без валидации

--- HW 3.1 Vitalii Kuzminskyi.py
+++ HW 3.1 Vitalii Kuzminskyi.py
@@ -1,54 +1,27 @@
 
-a = float(input('Пожалуйста введите первое число: ')) #использовал float для более широких возможностей и более точного расчёта
-b = float(input('Пожалуйста введите второе число: '))
-c = float(input('Пожалуйста введите третье число: '))
+while True:
+    try:
+        a = float(input('Пожалуйста введите первое число: '))
+        break
+    except:
+        print('Ввод символов кроме цифр ЗАПРЕЩЁН, введите первое число: ')
 
-# def main():
-#
-#     a = True
-#     while a:
-#         a = False
-#         float = input('Пожалуйста введите первое число: ')
-#         try:
-#             for i in float:
-#                 if i.isdigit() is not True:
-#                     raise ValueError("Первое число должно содержать только цифры")
-#         except Exception:
-#             print("Первое число должно содержать только цифры")
-#             a = True
-#
-#
-#
-#     b = True
-#     while b:
-#         b = False
-#         number2 = input('Пожалуйста введите второе число: ')
-#         try:
-#             for i in number2:
-#                 if i.isdigit() is not True:
-#                     raise ValueError("Второе число должно содержать только цифры")
-#         except Exception:
-#             print("Второе число должно содержать только цифры")
-#             b = True
-#
-#     c = True
-#     while c:
-#         c = False
-#         number3 = input('Пожалуйста введите третье число: ')
-#         try:
-#             for i in number3:
-#                 if i.isdigit() is not True:
-#                     raise ValueError("Третье число должно содержать только цифры")
-#         except Exception:
-#             print("Третье число должно содержать только цифры")
-#             c = True
-# if __name__ == '__main__':
-#     main()
+while True:
+    try:
+        b = float(input('Пожалуйста введите второе число: '))
+        break
+    except:
+        print('Ввод символов кроме цифр ЗАПРЕЩЁН, введите второе число: ')
 
-
+while True:
+    try:
+        c = float(input('Пожалуйста введите третье число: '))
+        break
+    except:
+        print('Ввод символов кроме цифр ЗАПРЕЩЁН, введите третье число: ')
 
 max = max(a, b, c)
-
+# z = int(max)
 if (a > 10 and b > 10 and c > 10) and ( a % 3 == 0 and b % 3 == 0): print (' Yes - Homework 3.1\n',max, '- Homework 3.2')
 
 else: print(' No - Homework 3.1\n', max, '- Homework 3.2')
