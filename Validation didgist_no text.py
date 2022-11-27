bcf2f992df8dfb50eaf08c0fa23507902d7d595b
Vitalii <vitalii.kuzminskyi@gmail.com>
2022-11-27 16:41:09 +0200

    HW 4 + validation

--- Validation didgist_no text.py
+++ Validation didgist_no text.py
@@ -7,19 +7,4 @@ while True:
             break
     except:
         print('ОШИБКА ВВОДА!!! Ввод только цифр!')
-n = base
-a = '*'
-b = '#'
-# print('\n' + 'HW 4.1')
-# for i in range(n, 0, -1):
-#     print(a * i)
-# print('HW 4.2')
-# for i in range(n + 1):
-#     print(a * i)
-print('HW 4.*1')
-for i in range(n + 1):
-      print((b * i),(a * i))
-
-
-
-
+print(f'Yes {base}')
