b54477344ff774de2c5bba1bbc19b511df1115c6
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2022-11-27 17:56:37 +0200

    задача с 1/4 часа

--- HW 4.1.py
+++ HW 4.1.py
@@ -10,15 +10,20 @@ while True:
 n = base
 a = '*'
 b = '#'
-# print('\n' + 'HW 4.1')
-# for i in range(n, 0, -1):
-#     print(a * i)
-# print('HW 4.2')
-# for i in range(n + 1):
-#     print(a * i)
+print('\n' + 'HW 4.1')
+for i in range(n, 0, -1):
+    print(a * i)
+print('HW 4.2', end='')
+for i in range(0, (n + 1)):
+    print(a * i)
 print('HW 4.*1')
-for i in range(n + 1):
-      print((b * i),(a * i))
+
+for i in range(0, (n + 1)):
+    for j in range(n, (i + 1), -1):
+        print((b * i) + (j * a))
+
+        break
+
 
 
 
