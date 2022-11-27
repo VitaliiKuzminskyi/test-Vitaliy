3b55118227ad091be9400725e6da30cd6038ded2
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2022-11-27 21:03:56 +0200

    HW 4 Done!

--- Validation didgist_no text.py
+++ Validation didgist_no text.py
@@ -1,10 +1,11 @@
 while True:
     try:
-        base = int(input('Введите размер основания треугольника от 2 до 10: '))
-        if 1 >= base or base > 10:
+        number = int(input('Введите размер основания треугольника от 2 до 10: '))
+        if 1 >= number or number > 10:
             pass
         else:
             break
     except:
         print('ОШИБКА ВВОДА!!! Ввод только цифр!')
-print(f'Yes {base}')
+
+print(f'Yes {number}')
