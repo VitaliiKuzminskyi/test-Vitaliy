27414c0cd0afa7ff6d1bbc9ac052447ac8a4515d
Vitalii <vitalii.kuzminskyi@gmail.com>
2022-11-28 22:10:08 +0200

    HW 5.1 + 5.2 +/-

---
+++ HW 5.py
@@ -0,0 +1,18 @@
+# HW 5.1
+# print('HW 5.1')
+# hw_5_1= input('Введите слово: ')
+# a = hw_5_1[0:]
+# b = hw_5_1[::-1]
+# if a == b:
+#     print('Result HW 5.1: +\n')
+# else:
+#     print('Result HW 5.1: -\n')
+
+# HW 5.2
+print('HW 5.2')
+hw_5_2_text = input('Введите текст: ')
+hw_5_2_f_word = input('Введите слово: ')
+if not hw_5_2_text.find(hw_5_2_f_word) == -1:
+    print('Result HW 5.2: YES')
+else:
+    print('Result HW 5.2: NO')
