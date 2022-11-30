5e1441161768de2db7a01d3e4fb8ed729c91d719
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2022-11-30 18:01:03 +0200

    HW 7 7.1 ++/- 7.2 +

--- HW 7.py
+++ HW 7.py
@@ -1,16 +1,24 @@
                                                          # HW 7.1
+# print('HW 7.1 - Запросить у пользователя 5 чисел и записать их в список')
+# numbers = []
+# n = '5'
+# while True:
+#     try:
+#         for i in range(5):
+#             number = int(input(f'Введите {n} чисел. Введите {i + 1} число: '))
+#             numbers.append(number)
+#         break
+#     except:
+#         print(f'Ввод символов кроме цифр ЗАПРЕЩЁН!!! Введите {n} чисел. Введите {i + 1} число: : ')
+#         # number = int(input(f'Введите {n} чисел. Введите {i + 1} число: '))
+# print(f'Result HW 7.1: {numbers} \n')
 
-print('HW 7.1 - Запросить у пользователя 5 чисел и записать их в список')
+                                                         # HW 7.2
 
-numbers = []
-n = '5'
-for i in range(5):
-    number = input(f'Введите {n} чисел. Введите {i+1} число: ')
-    if number.isdigit():
-        numbers.append(number)
+print('HW 7.2 - Дан список A = [1, 2, 3, 4, 5] Удалить последнее число из списка')
+list2 = ['1', '2', '3', '4', '5']
+a = list2[-1]
+list2.remove(a)
+print(f'Result HW 7.2: {list2} \n')
 
-
-    else:
-        break
-
-print(f'Result HW 6.1: {numbers} \n')
+                                                         # HW 7.3
