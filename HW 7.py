fff02c57e03b3a67ddff6fcf0658ef1b51ff0abb
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2022-11-30 21:53:34 +0200

    HW 7 7.1 + 7.2 + 7.3 + 7.4 + 7.5

--- HW 7.py
+++ HW 7.py
@@ -1,58 +1,70 @@
                                                          # HW 7.1
-# print('HW 7.1 - Запросить у пользователя 5 чисел и записать их в список')
+# print('HW 7.1')
 # numbers = []
 # n = 5
 # while True:
 #     try:
 #         for i in range(0, n):
-#             number = int(input(f'Введите {n} чисел. Введите {i + 1} число: '))
+#             number = int(input(f'Введите {i + 1} число из {n}: '))
 #             numbers.append(number)
 #         break
 #     except:
-#         print(f'Ввод символов кроме цифр ЗАПРЕЩЁН!!! Введите {n} чисел. Введите {i + 1} число: : ')
-#         # number = int(input(f'Введите {n} чисел. Введите {i + 1} число: '))
+#         print(f'Ввод символов кроме цифр ЗАПРЕЩЁН!!! Введите {i + 1} число из {n}: ')
+#         numbers.clear()
 # print(f'Result HW 7.1: {numbers} \n')
 
                                                          # HW 7.2
 
-# print('HW 7.2 - Дан список A = [1, 2, 3, 4, 5] Удалить последнее число из списка')
-# list2 = ['1', '2', '3', '4', '5']
+# print('HW 7.2')
+# list2 = [1, 2, 3, 4, 5]
 # a = list2[-1]
 # list2.remove(a)
 # print(f'Result HW 7.2: {list2} \n')
 
                                                          # HW 7.3
-# print('HW 7.3 - Запросить у пользователя 10 чисел и записать их в список A\n'
-#       'Запросить у пользователя число N\n'
-#       'Вывести пользователю сколько в списке A повторяется число N')
-#
+# print('HW 7.3')
 # numbers = []
 # a = 10
 # while True:
 #     try:
 #         for i in range(0, a):
-#             number = int(input(f'Введите {a} чисел. Введите {i + 1} число: '))
+#             number = int(input(f'Введите {i + 1} число из {a}: '))
 #             numbers.append(number)
 #         break
 #     except:
-#         print(f'Ввод символов кроме цифр ЗАПРЕЩЁН!!! Введите {a} чисел. Введите {i + 1} число: : ')
+#         print(f'Ввод символов кроме цифр ЗАПРЕЩЁН!!! Введите {i + 1} число из {a}: ')
+#         numbers.clear()
 #
 # while True:
 #     try:
 #         n = int(input('Введите искомое число: '))
 #         break
 #     except:
-#         print(f'Искомое число должно состоять из цифр:')
+#         print(f'Искомое число должно состоять из цифр: ')
 # b = numbers.count(n)
 # print(f'Result HW 7.3. В Вашем списке {b} искомых \'{n}\' \n')
 
                                                          # HW 7.4
-print('HW 7.4 - Запросить у пользователя число N \n'
-      'Запросить у пользователя N чисел и записать их в список A\n'
-      'Вывести список в обратной последовательности')
+# print('HW 7.4')
+# while True:
+#     try:
+#         n = int(input('Количество чисел в списке: '))
+#         break
+#     except:
+#         print(f'Ввод только цифр: ')
+#
+# numbers = []
+# while True:
+#     try:
+#         for i in range(0, n):
+#             number = int(input(f'Введите {i + 1} число из {n}: '))
+#             numbers.append(number)
+#         break
+#     except:
+#         print(f'Ввод символов кроме цифр ЗАПРЕЩЁН!!! Введите {i + 1} число из {n}: ')
+#         numbers.clear()
+# numbers.reverse()
+# print(f'Result HW 7.4. Ваши числа в обратном порядке: {numbers} \n')
 
-
-
-print(f'Result HW 7.4. Ваш обратный список введеных чисел {} \n')
-
+                                                         # HW 7.5
 
