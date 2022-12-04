1084c0520d32dfe580378b827608866bd76a70cc
Vitalii <vitalii.kuzminskyi@gmail.com>
2022-12-04 15:41:36 +0200

    HW 8.1 + 8.2 ++-

--- HW 8.py
+++ HW 8.py
@@ -7,7 +7,6 @@
 
                                                          # HW 8.2
 print('HW 8.2')
-
 while True:
     try:
         n = int(input('Введите количество повреждённых файлов: '))
@@ -15,22 +14,22 @@ while True:
     except:
         print('ОШИБКА!!! Ввод только целых чисел: ')
 
-permissions = {}
+
 print('Действия с файлами:\n R - чтение; \n W - запись; \n E - выполненение.')
+permissions = {}
 for i in range(n):
-    s = input(f'Из {n} файлов введите имя {i+1}-го файла и действия с ним (например readme.txt R W): ').split()
-    permissions[s[0]] = set(s[1:])
+    f = input(f'Из {n} файлов введите имя {i+1}-го файла и действия с ним (например readme.txt R W): ').split()
+    permissions[f[0]] = set(f[1:])
 
 
 for j in range(int(input('Введите количество действий над файлами: '))):
-        print('Действия с файлами:\n read - чтение; \n write - запись; \n execute - выполненение.')
-        permis, file = input('Запросите действие над файлом и его имя (например: read [имя файла]): ').split()
+        permis, file = input('Запросите действие (read/write/execute) над файлом и его имя (например: read [имя файла]): ').split()
         if permis == 'read':
             permis = 'R'
         if permis == 'write':
             permis = 'W'
         if permis == 'execute':
-            permis = 'X'
+            permis = 'E'
         if permis in permissions[file]:
             print('OK')
         else:
