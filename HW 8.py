a82e099bc0174df70202faf75d4668ac5d7bc81f
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2022-12-04 21:37:39 +0200

    HW 8 DONE!

--- HW 8.py
+++ HW 8.py
@@ -1,9 +1,9 @@
 
-                                                         # HW 8.1
-# print('HW 8.1')
-# a = {'Dmitriy', 'Nikolay', 'Vasyliy', 'Tatyana', 'Masha', 'Natasha'}
-# b = {'Anton', 'Dmitriy', 'Vladimir', 'Alexey', 'Masha', 'Natasha', 'Olga'}
-# print(f' За сентябрь и октябрь должны: { sorted(a.union(b))}, \n Только за октябрь должны: { sorted(b.difference(a))}')
+                                                         HW 8.1
+print('HW 8.1')
+a = {'Dmitriy', 'Nikolay', 'Vasyliy', 'Tatyana', 'Masha', 'Natasha'}
+b = {'Anton', 'Dmitriy', 'Vladimir', 'Alexey', 'Masha', 'Natasha', 'Olga'}
+print(f' За сентябрь и октябрь должны: { sorted(a.union(b))}, \n Только за октябрь должны: { sorted(b.difference(a))}')
 
                                                          # HW 8.2
 print('HW 8.2')
