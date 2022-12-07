dcc11b4131769466257758064d883370ddb4d2d1
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2022-12-07 18:47:40 +0200

    HW 9 9.1 + 9.2 + 9.3 + 9.4 -+ 9.5 -

--- HW 9.py
+++ HW 9.py
@@ -48,33 +48,46 @@
 # hw9_3.write(hw9_3_done)
 
 #                                                          # HW9_4
-#
-#
+
 # print('HW 9.4')
-# rundom_numb = rundom
-# print(rundom_numb)
+# import random
+# rec_numbers = []
+# n = 100
+# a = '\n'
+# for i in range(n):
+#     f = random.randint(0, 200)
+#     rec_numbers.append(f)
+# print(rec_numbers)
+# with open('hw9_4_numbers.txt', 'w') as file:
+#     for j in rec_numbers:
+#         file.write(j + a)
+# print(rec_numbers)
+#
+#                                                          # HW9_5
 
-                                                         # HW9_5
+
+# print('HW 9.5')
+# hw9_5 = open('hw9_5.txt', 'r')
+# text = hw9_5.read()
+#
+# words = []
+# numbers = []
+# for i in text:
+#     if i.isnumeric():
+#         if not numbers or not last_isnumeric:
+#             numbers.append(i)
+#         else:
+#             numbers[-1] += i
+#     else:
+#         if not words or last_isnumeric:
+#             words.append(i)
+#         else:
+#             words[-1] += i
+#     last_isnumeric = i.isnumeric()
+#
+# print(words)
+# print(len(words))
 
 
-print('HW 9.5')
-hw9_5 = open('hw9_5.txt', 'r')
-text = hw9_5.read()
 
-words = []
-numbers = []
-for i in text:
-    if i.isnumeric():
-        if not numbers or not last_isnumeric:
-            numbers.append(i)
-        else:
-            numbers[-1] += i
-    else:
-        if not words or last_isnumeric:
-            words.append(i)
-        else:
-            words[-1] += i
-    last_isnumeric = i.isnumeric()
 
-print(words)
-print(len(words))
