90d8cb732af20fe71986330da2b92385b2e00241
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2022-12-19 08:00:29 +0200

    HW 13 -

---
+++ HW 13.py
@@ -0,0 +1,10 @@
+
+                                                                    # HW 13
+
+class Human:
+
+    def __init__(self, Name: str, Surname: str, Age: int, Phone: int, Address: str):
+
+    def get_info(self):
+        print('Hello!')
+
