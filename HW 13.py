2851e2eedfed01815dee074af44e93b5ed6e9de6
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2022-12-19 12:07:01 +0200

    HW 13 +-

--- HW 13.py
+++ HW 13.py
@@ -13,17 +13,19 @@ class Human:
         print(f'{self.name}, {self.surname}, {self.age}, {self.phone}, {self.adress}')
         pass
 
-    def __call__(self, phone):
-        print({self.phone})
+    def call(self):
+        print(f'{self.phone}')
+        pass
 
 
 victor = Human('Victor', 'Popov', 31, '+38 067 961 08 45', 'm.Kharkiv, Sumska str. 12, app 14')
-
 olha = Human('Olha', 'Volk', 24, '+38 099 123 88 99', 'm.Zaporizsha, Stepana Bendery str. 118')
-
 alexey = Human('Alexey', 'Nosin', 44, '+38 063 236 47 58', 'm.Lviv, Striyska str. 53, app 236')
 
 
 victor.get_info()
 olha.get_info()
 alexey.get_info()
+
+victor.call()
+
